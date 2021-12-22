9d606316300f722935eece4e977966db86c25e03
DenisFTJunior <junior.denis2354@outlook.com>
2021-12-22 14:17:25 -0300

    error found in querys

---
+++ src/components/form/AvatarSelect.tsx
@@ -0,0 +1,5 @@
+const AvatarSelect = () => {
+    return <></>
+}
+
+export default AvatarSelect
---
+++ src/utils/graphql/mutation/rickAndMorty/schema.ts
@@ -0,0 +1,9 @@
+export interface Character {
+    readonly id: string,
+    readonly name: string,
+    readonly status: string,
+    readonly gender: string,
+    readonly image: string,
+    readonly species: string,
+    readonly [propName: string]: any
+}
